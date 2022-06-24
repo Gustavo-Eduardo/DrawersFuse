976d53fd81042226d428d660a793ecd3e517204e
Gustavo-Eduardo <guedlope@espol.edu.ec>
2022-06-23 19:16:17 -0500

    ya

--- src/Drawer/Drawer.js
+++ src/Drawer/Drawer.js
@@ -12,114 +12,138 @@ import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import DrawerCard from '../Cards/DrawerCard';
+import "./Drawer.css"
 
 const drawerBleeding = 56;
 
 const Root = styled('div')(({ theme }) => ({
-  height: '100%',
-  backgroundColor:
-    theme.palette.mode === 'light' ? grey[100] : theme.palette.background.default,
+    height: '100%',
+    backgroundColor:
+        theme.palette.mode === 'light' ? grey[100] : theme.palette.background.default,
 }));
 
 const StyledBox = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
+    backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
 }));
 
 const Puller = styled(Box)(({ theme }) => ({
-  width: 30,
-  height: 6,
-  backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
-  borderRadius: 3,
-  position: 'absolute',
-  top: 8,
-  left: 'calc(50% - 15px)',
+    width: 30,
+    height: 6,
+    backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
+    borderRadius: 3,
+    position: 'absolute',
+    top: 8,
+    left: 'calc(50% - 15px)',
 }));
 
 function SwipeableEdgeDrawer(props) {
-  const { window } = props;
-  const [open, setOpen] = React.useState(false);
+    const { window } = props;
+    const [open, setOpen] = React.useState(false);
 
-  const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
-  };
+    const toggleDrawer = (newOpen) => () => {
+        setOpen(newOpen);
+    };
 
-  // This is used only for the example
-  const container = window !== undefined ? () => window().document.body : undefined;
+    // This is used only for the example
+    const container = window !== undefined ? () => window().document.body : undefined;
 
-  return (
-    <Root onMouseOver={toggleDrawer(true)}>
-      <CssBaseline />
-      <Global
-      
-        styles={{
-          '.MuiDrawer-root > .MuiPaper-root': {
-            height: `calc(25% - ${drawerBleeding}px)`,
-            overflow: 'visible',
-          },
-        }}
-      />
-      <SwipeableDrawer
-        container={container}
-        anchor="bottom"
-        open={open}        
-        onClose={toggleDrawer(false)}
-        onOpen={toggleDrawer(true)}
-        swipeAreaWidth={drawerBleeding}
-        disableSwipeToOpen={false}
-        ModalProps={{
-          keepMounted: true,
-        }}
-      >
-        <StyledBox 
-        
-          sx={{
-            position: 'absolute',
-            top: -drawerBleeding,
-            borderTopLeftRadius: 8,
-            borderTopRightRadius: 8,
-            visibility: 'visible',
-            right: 0,
-            left: 0,
-          }}
-        >
-          <Puller />          
-          <Typography sx={{ p: 2, color: 'text.secondary' }}>51 results</Typography>
-          <Box sx={{display:"flex", px:6, gap:5}}>
-            <DrawerCard dataIds={{
-                title: "Start",
-                desc: `
-                Profit: $50000
+    return (
+        <div onMouseOver={toggleDrawer(true)}>
+            {/* <Global
+
+                styles={{
+                    '.MuiDrawer-root > .MuiPaper-root': {
+                        height: `calc(25% - ${drawerBleeding}px)`,
+                        overflow: 'visible',
+                    },
+                }}
+            /> */}
+            <SwipeableDrawer
+                container={container}
+                anchor="bottom"
+                open={open}
+                onClose={toggleDrawer(false)}
+                onOpen={toggleDrawer(true)}
+                swipeAreaWidth={drawerBleeding}
+                disableSwipeToOpen={false}
+                ModalProps={{
+                    keepMounted: true,
+                }}
+            >
+                <StyledBox
+                    style={{
+                        maxHeight: 200,
+                        overflow: "auto"
+                    }}
+
+                    sx={{
+                        position: 'absolute',
+                        top: -drawerBleeding,
+                        borderTopLeftRadius: 8,
+                        borderTopRightRadius: 8,
+                        visibility: 'visible',
+                        right: 0,
+                        left: 0,
+                    }}
+                >
+                    <Puller />
+                    <Typography sx={{ p: 2, color: 'text.secondary' }}>51 results</Typography>
+                    <Box sx={{ display: "flex", flexWrap: "wrap", px: 6, gap: 5 }}>
+                        <DrawerCard dataIds={{
+                            title: "Start",
+                            desc: `
+                Profit: 137.34
+
 
                 `
-            }}/>
-            <Divider orientation="vertical"/>
-            <DrawerCard dataIds={{
-                title: "Hola",
-                desc: "Como estas"
-            }}/>
-            
-          </Box>
-        </StyledBox>
-        <StyledBox 
-          sx={{
-            px: 2,
-            pb: 2,
-            height: '100%',
-            overflow: 'auto',
-          }}
-        >
-        </StyledBox>
-      </SwipeableDrawer>
-    </Root>
-  );
+                        }} />
+                        <DrawerCard dataIds={{
+                            title: "Stop 1",
+                            desc: "Profit: 137.34"
+                        }} />
+                        <DrawerCard dataIds={{
+                            title: "Stop 2",
+                            desc: "Profit: 137.34"
+                        }} />
+                        <DrawerCard dataIds={{
+                            title: "End",
+                            desc: "Profit: 137.34"
+                        }} />
+                        <DrawerCard dataIds={{
+                            title: "End",
+                            desc: "Profit: 137.34"
+                        }} />
+                        <DrawerCard dataIds={{
+                            title: "End",
+                            desc: "Profit: 137.34"
+                        }} />
+                        <DrawerCard dataIds={{
+                            title: "End",
+                            desc: "Profit: 137.34"
+                        }} />
+
+                    </Box>
+                </StyledBox>
+                <StyledBox
+                    sx={{
+                        px: 2,
+                        pb: 2,
+                        height: '100%',
+                        overflow: 'auto',
+                    }}
+                >
+                </StyledBox>
+            </SwipeableDrawer>
+        </div>
+    );
 }
 
 SwipeableEdgeDrawer.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
 };
 
 export default SwipeableEdgeDrawer;
